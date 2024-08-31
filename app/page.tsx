c44bd33be2e41df3ee23868a487f63a5c25761ca
Swarnendu <clickswarnendu123@gmail.com>
2024-08-31 10:56:59 +0530

    mingrted into next

---
+++ app/page.tsx
@@ -0,0 +1,57 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+import {
+  NavigationMenuLink,
+} from "@/components/ui/navigation-menu"
+
+
+
+import { CardWithForm } from "@/components/core/newproject"
+import { ProjectList } from "@/components/core/projectlist"
+import { NavigationMenuComponent } from "@/components/core/navigation"
+
+
+
+export default function NavigationMenuDemo() {
+  return (
+    <div>
+      <NavigationMenuComponent />
+
+      <div className="flex justify-evenly">
+        <div className="">
+          <ProjectList />
+        </div>
+        <div className="m-5">
+          <CardWithForm />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
